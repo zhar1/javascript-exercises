d3924318335228a1f8b787657abf664d316b9aec
Zaire <110312083+zhar1@users.noreply.github.com>
2023-02-17 15:46:55 -0600

    removes target value(s) from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numbers, ...targets) {
+  //create temporary array for return value 
+  let new_arr = numbers;
+  //loop through targets
+  for (const target of targets){
+    //filter current target out and assign to temporary array
+    new_arr = new_arr.filter(notTarget);
+    //returns value != target value
+    function notTarget(num){
+        if (num !== target){
+            return num;
+        }
+    }
+   }
+ //returns final modified array
+return new_arr;
 };
 
 // Do not edit below this line
