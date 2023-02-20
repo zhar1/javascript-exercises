4f8707acbd6175e53be05077a239ce6f55d72938
Zaire <110312083+zhar1@users.noreply.github.com>
2023-02-19 23:16:05 -0600

    converts degrees celsius to farenheit converts degrees farenheitto celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let celcius = (num - 32) * (5/9);
+  return (celcius % 1 === 0 )? celcius: +celcius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let fahrenheit = ((num * (9/5)) + 32);
+  return (fahrenheit % 1 === 0)? fahrenheit: +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
