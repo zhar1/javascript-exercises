2a7dfe64b2fa30489bf92abffd1885fdbfc89078
Zaire <110312083+zhar1@users.noreply.github.com>
2023-02-19 21:38:38 -0600

    retruns true if year is a leapyear returns fals if year is not leapyear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(a) {
+let year = a;
+//leap years are divisible by 4 but not by 100 or divisible by 400
+if (((year%4 === 0) && (year%100 > 0)) || (year%400 === 0)){
+    return true;
+} else {return false;}
 };
 
 // Do not edit below this line
