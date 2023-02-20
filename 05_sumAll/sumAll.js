6929f12cee66f406d05d156ee5f564a6d8cff571
Zaire <110312083+zhar1@users.noreply.github.com>
2023-02-19 21:13:15 -0600

    returns the inclusive sum between non-negative integers a and b

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+//check if parameters are integer and non-negative
+if ((Number.isInteger(a) && Number.isInteger(b)) && (a >= 0 && b >= 0)){
+    const first_num = (a < b)? a: b;
+    const last_num = (a > b)? a: b;
+    let sum = 0;
+    for (i = first_num; i <= last_num; i++){
+        sum += i;
+    }
+    return sum;
+} else {return 'ERROR';}
 };
 
 // Do not edit below this line
